cbd5d35bcc1fb802c26d37d131be3d806771dcff
Quentin Champenois <quentin.champenois7@gmail.com>
2025-07-01 14:30:45 +0200

    Revert "revert: Add Vercel Provider"
    This reverts commit 679a3b2542fae8b572ce9494193bc387384b5dca.

--- infra/providers.tf
+++ infra/providers.tf
@@ -5,10 +5,6 @@ terraform {
       source  = "digitalocean/digitalocean"
       version = "~> 2.0"
     }
-    vercel = {
-      source  = "vercel/vercel"
-      version = "~> 2.0"
-    }
     kubernetes = {
       source = "hashicorp/kubernetes"
     }
@@ -40,11 +36,3 @@ provider "digitalocean" {
   spaces_secret_key = var.spaces_secret_key # DO Spaces Secret Key
 }
 
-provider "vercel" {
-  # Or omit this for the api_token to be read
-  # from the VERCEL_API_TOKEN environment variable
-  api_token = var.vercel_api_token
-
-  # Optional default team for all resources
-  team = "dembrane"
-}
